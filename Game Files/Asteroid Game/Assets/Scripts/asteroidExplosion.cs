809994f198c6f41eaa460d906d59846d4464f724
Ochamberlai2 <S179650@ucs.ac.uk>
2017-02-20 14:47:46 +0000

    Added amy's asteroid sprites to the project

--- Game Files/Asteroid Game/Assets/Scripts/asteroidExplosion.cs
+++ Game Files/Asteroid Game/Assets/Scripts/asteroidExplosion.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class asteroidExplosion : MonoBehaviour {
 
     public GameObject asteroidPiece;
+    private int asteroidPieceAmount = 4;
     
 
 	// Use this for initialization
@@ -13,17 +14,20 @@ public class asteroidExplosion : MonoBehaviour {
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-
+        //instantiate asteroid x number of times
         if (collision.collider.tag == "rocketT2")
         {
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < asteroidPieceAmount; i++)
             {
+               
                 GameObject asteroid = Instantiate(asteroidPiece, gameObject.transform.position, transform.rotation) as GameObject;
-                asteroid.transform.localScale = new Vector3((gameObject.transform.localScale.x / 4), (gameObject.transform.localScale.y / 4), (gameObject.transform.localScale.z / 4));
+                //make the asteroid's scale equal to 1/3 of the original asteroid
+                asteroid.transform.localScale = new Vector3((gameObject.transform.localScale.x / 3), (gameObject.transform.localScale.y / 3), (gameObject.transform.localScale.z / 3));
+                //randomise velocity and give it mass equal to 1/4 of the original 
                 asteroid.GetComponent<Rigidbody2D>().velocity = new Vector3(Random.Range(-6f, 6f), Random.Range(-6f, 6f), 0);
                 asteroid.GetComponent<Rigidbody2D>().mass = gameObject.GetComponent<Rigidbody2D>().mass / 4;     
             }
-            gameObject.SetActive(false);
+            Destroy(gameObject);
         }
     }
 }
--- Game Files/Asteroid Game/Assets/Scripts/asteroidSpawner.cs
+++ Game Files/Asteroid Game/Assets/Scripts/asteroidSpawner.cs
@@ -7,19 +7,31 @@ public class asteroidSpawner : MonoBehaviour {
     public GameObject asteroidprefab;
     private int maxAsteroids = 30;
     private GameObject[] asteroidArr;
+
+    public Sprite asteroidv1;
+    public Sprite asteroidv2;
+    public Sprite asteroidv3;
+
+
     // Use this for initialization
     void Start() {
         parentObj = gameObject;
+
         asteroidArr = new GameObject[maxAsteroids];//initialise an array to store all asteroid objects
         for (int i = 0; i < maxAsteroids; i++)
         {
+            int randomAstType = Random.Range(0, 2);
             float randomSize = Random.Range(1f, 2f);//random scale number for localscale
             GameObject asteroid = Instantiate(asteroidprefab, new Vector3(Random.Range(-19f, 19f), Random.Range(-19f, 19f), 0), Quaternion.identity) as GameObject;//instantiate game object with a random x,y
             asteroid.transform.localScale =new Vector3(randomSize ,randomSize,0);//random x,y scale between 1 and 2 (floating point numbers)
-            asteroid.GetComponent<CircleCollider2D>().radius = randomSize;
+            asteroid.GetComponent<CircleCollider2D>().radius = randomSize*0.75f;
             asteroid.GetComponent<Rigidbody2D>().mass = randomSize;
             asteroid.transform.parent = parentObj.transform;
             asteroidArr[i] = asteroid;//assign position in array
+
+            if (randomAstType == 0) asteroid.GetComponent<SpriteRenderer>().sprite = asteroidv1;
+            if (randomAstType == 1) asteroid.GetComponent<SpriteRenderer>().sprite = asteroidv2;
+            if (randomAstType == 2) asteroid.GetComponent<SpriteRenderer>().sprite = asteroidv3;
         }
     }
 }
