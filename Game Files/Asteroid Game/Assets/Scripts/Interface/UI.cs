e32a072a0882161b89463e60a786fd464d791e5d
Oliver Chamberlain <S179650@ucs.ac.uk>
2017-03-20 21:24:24 +0000

    fixed ui

--- Game Files/Asteroid Game/Assets/Scripts/Interface/UI.cs
+++ Game Files/Asteroid Game/Assets/Scripts/Interface/UI.cs
@@ -33,22 +33,22 @@ public class UI : MonoBehaviour {
         //when health is full, set all sprites 
         if (healthControl.Health == 5)
         {
-           HP5.GetComponent<SpriteRenderer>().sprite = S_HP;
-           HP4.GetComponent<SpriteRenderer>().sprite = S_HP;
-           HP3.GetComponent<SpriteRenderer>().sprite = S_HP;
-           HP2.GetComponent<SpriteRenderer>().sprite = S_HP;
-           HP1.GetComponent<SpriteRenderer>().sprite = S_HP;
+            HP5.GetComponent<SpriteRenderer>().sprite = S_HP;
+            HP4.GetComponent<SpriteRenderer>().sprite = S_HP;
+            HP3.GetComponent<SpriteRenderer>().sprite = S_HP;
+            HP2.GetComponent<SpriteRenderer>().sprite = S_HP;
+            HP1.GetComponent<SpriteRenderer>().sprite = S_HP;
         }
         //when health is 4, show only 4 sprites
-        else if (healthControl.Health == 4) HP5.GetComponent<SpriteRenderer>().sprite = null;
+        else if (healthControl.Health == 4) HP5.SetActive(false);
         //when health is 3, show only 3 sprites 
-        else if (healthControl.Health == 3) HP4.GetComponent<SpriteRenderer>().sprite = null;
+        else if (healthControl.Health == 3) HP4.SetActive(false);
         //when health is 2, show only 2 sprites
-        else if (healthControl.Health == 2) HP3.GetComponent<SpriteRenderer>().sprite = null;
+        else if (healthControl.Health == 2) HP3.SetActive(false);
         //when health is 1, show only 1 sprites
-        else if (healthControl.Health == 1) HP2.GetComponent<SpriteRenderer>().sprite = null;
+        else if (healthControl.Health == 1) HP2.SetActive(false);
         //when health is 0, show no sprites
-        else if (healthControl.Health == 0) HP1.GetComponent<SpriteRenderer>().sprite = null;
+        else if (healthControl.Health == 0) HP1.SetActive(false);
 
     }
 }
